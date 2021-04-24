252efe15fbd32483685f41b89e656357f35a8365
gozde <gozdeoztoprak0@gmail.com>
2021-04-24 17:30:32 +0200

    cors header add

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 app.use(cors({
   origin: "https://api.nomics.com/v1/currencies/ticker?key=39229126e722ab40066b13018df86143&convert=USD"
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "https://api.nomics.com/v1/currencies/ticker?key=39229126e722ab40066b13018df86143&convert=USD");
 //   res.header(
